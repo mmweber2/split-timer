042fad4cac7a248c973435444239de6bdfabe98e
Mary Ellen Weber <mmweber2@gmail.com>
2018-07-12 13:10:20 -0500

    First commit
    Create base file for timer class.

---
+++ timer.py
@@ -0,0 +1,16 @@
+class Timer:
+
+    def __init__(self):
+        self.events = []
+        self.event_names = {}
+        self.overall_time = 0.0
+        self.current_event_id = 0
+
+    def add_event(self, name):
+        pass
+
+    def split(self):
+        pass
+
+    def _get_time_delta(self, event_id, new_time):
+        pass
